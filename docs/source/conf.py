4a4874dd29f432560826b076fa9ea2d94aafab23
Luis Vasquez <luis.vasquez@bsc.es>
2025-01-22 18:16:55 +0100

    Test GHPages workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,10 +21,12 @@ import os
 import sys
 #sys.path.insert(0, os.path.abspath('..'))
 #sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
-import sys
 
 print("EXECUTABLE")
-print(sys.executable)
+print("HERE:",sys.executable)
+import time
+time.sleep(5)
+
 import rl_salamandra_alignment
 
 # -- General configuration ---------------------------------------------
